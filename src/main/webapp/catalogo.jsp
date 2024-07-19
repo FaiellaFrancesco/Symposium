37978fb4e85ee3531b0a2a7ec45ed3b715c3b785
Giuseppe Gambardella <gagiuseppe29@gmail.com>
2024-07-19 19:00:54 +0200

    migliorato il css del catalogo con animazione a carta per i prodotti

--- src/main/webapp/catalogo.jsp
+++ src/main/webapp/catalogo.jsp
@@ -25,42 +25,57 @@
 <%@ include file="utilities/header.jsp" %>
 
 <body>
-	
-	<!-- Campi di filtraggio -->
-	<div class="filter">
-	    	<a onclick="redirectToACatalogo()">Mostra tutto</a>
-	    	<a onclick="filtro('Vini Rossi')">Vini Rossi</a>
-	    	<a onclick="filtro('Vini Bianchi')">Vini Bianchi</a>
-	    	<a onclick="filtro('Vini Rosati')">Vini Rosati</a>
-	    	<a onclick="filtro('Champagne')">Champagne</a>
-	    	<a onclick="filtro('Spumanti')">Spumanti</a>
-	    	<a onclick="filtro('Altro')">Altri prodotti</a>
-	</div>
-
- <% if (products != null && products.size() != 0) { %>
-<div id=container>
-<div class="grid-container">
-  <%
-    Iterator<?> it = products.iterator();
-    while (it.hasNext()) {
-      Prodotto bean = (Prodotto) it.next();
-  %>
-  <div class="product-container" onclick="redirectToProduct(<%= bean.getId() %>)">
-    <img class="product-image" src="<%= bean.getImmagine() %>" alt="<%= bean.getNome() %>">
-    <h3 class="nome"><%= bean.getNome() %></h3>
-    <div class="details">
-      <p class="price"><%= String.format("%.2f", bean.getPrezzo()) %>€ </p>
-      <a href="ControlloProdotto?action=addToC&id=<%= bean.getId() %>&quantity=1" class="carrello-button">Carrello</a>
+    <!-- Campi di filtraggio -->
+    <div class="filter">
+        <a onclick="redirectToACatalogo()">Mostra tutto</a>
+        <a onclick="filtro('Vini Rossi')">Vini Rossi</a>
+        <a onclick="filtro('Vini Bianchi')">Vini Bianchi</a>
+        <a onclick="filtro('Vini Rosati')">Vini Rosati</a>
+        <a onclick="filtro('Champagne')">Champagne</a>
+        <a onclick="filtro('Spumanti')">Spumanti</a>
+        <a onclick="filtro('Altro')">Altri prodotti</a>
     </div>
-  </div>
-  <% } %>
-  </div>
-</div>
-      <% } else { %>
-      <div class="error-message">
- 		 <p>Nessun prodotto disponibile al momento.</p>
-	</div>
-	<% }  %>
- <footer><%@ include file="utilities/footer.jsp" %></footer>
+
+    <% if (products != null && products.size() != 0) { %>
+    <div id="container">
+        <div class="grid-container">
+            <%
+                Iterator<?> it = products.iterator();
+                while (it.hasNext()) {
+                    Prodotto bean = (Prodotto) it.next();
+            %>
+            <div class="product-card">
+                <div class="product-container" onclick="redirectToProduct(<%= bean.getId() %>)">
+                    <div class="product-front">
+                        <img class="product-image" src="<%= bean.getImmagine() %>" alt="<%= bean.getNome() %>">
+                        <h3 class="nome"><%= bean.getNome() %></h3>
+                        <div class="details">
+                            <p class="price"><%= String.format("%.2f", bean.getPrezzo()) %>€ </p>
+                        </div>
+                    </div>
+                    <div class="product-back">
+                        <!-- Retro della carta -->
+					    <div class="product-info">
+					        <p><b>Tipologia: </b><%= bean.getTipologia() %></p>
+					        <p><b>Provenienza: </b><%= bean.getProvenienza() %></p>
+					        <p><b>Denominazione: </b><%= bean.getDenominazione() %></p>
+					        <p><b>Annata: </b><%= bean.getAnnata() %></p>
+					        <p><b>Alcol: </b><%= bean.getAlcol() %> %</p>
+					        <p><b>Formato: </b><%= bean.getFormato() %> cl</p>
+					        <!-- Aggiungi altre informazioni se necessario -->
+					    </div>
+                        <a href="ControlloProdotto?action=addToC&id=<%= bean.getId() %>&quantity=1" class="carrello-button">Aggiungi al Carrello</a>
+                    </div>
+                </div>
+            </div>
+            <% } %>
+        </div>
+    </div>
+    <% } else { %>
+    <div class="error-message">
+        <p>Nessun prodotto disponibile al momento.</p>
+    </div>
+    <% } %>
+    <footer><%@ include file="utilities/footer.jsp" %></footer>
 </body>
 </html>
--- src/main/webapp/utilities/js/ajax.js
+++ src/main/webapp/utilities/js/ajax.js
@@ -3,130 +3,205 @@
  */
 
  
-function filtro(tipo){
-	var tipoVino = tipo;
-            var xhr = new XMLHttpRequest();
-            var basePath = window.location.origin + "/Symposium/";
-            xhr.open("GET", basePath + "catalogoAjax?type=" + encodeURIComponent(tipoVino), true);
-            xhr.onreadystatechange = function() {
-                if (xhr.readyState === 4 && xhr.status === 200) {
-					var container = document.getElementById('container');
-		            container.innerHTML="";
-		            var grid = document.createElement('div');
-					grid.classList.add('grid-container');
-                    var prodotti = JSON.parse(xhr.responseText);
-                    prodotti.forEach(function(prodotto){
-					var productcontainer = document.createElement("div");
-					productcontainer.classList.add("product-container");
-					productcontainer.addEventListener('click',function() {
-				        redirectToProduct(prodotto.id);
-				    });
-					
-            // Creazione del div principale del prodotto
+function filtro(tipo) {
+    var tipoVino = tipo;
+    var xhr = new XMLHttpRequest();
+    var basePath = window.location.origin + "/Symposium/";
+    xhr.open("GET", basePath + "catalogoAjax?type=" + encodeURIComponent(tipoVino), true);
+    
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            var container = document.getElementById('container');
+            container.innerHTML = ""; // Pulisce il contenuto esistente
 
-            // Creazione dell'elemento immagine
-            		var img = document.createElement('img');
-            		img.classList.add("product-image");
-            		img.src = prodotto.immagine;
-            		img.alt = prodotto.nome;
+            var grid = document.createElement('div');
+            grid.classList.add('grid-container');
 
-            // Creazione del titolo (h3)
-           			var title = document.createElement('h3');
-            		title.textContent = prodotto.nome;
-            		title.classList.add("nome");
-				
-					var details = document.createElement('div');
-					details.classList.add('details');	
-			
-            // Creazione del paragrafo per il prezzo
-            		var price = document.createElement('p');
-            		price.classList.add("price");
-            		price.textContent = prodotto.prezzo.toFixed(2) +"€ ";
-            		details.appendChild(price);
-
-            // Creazione del link per il pulsante 'Carrello'
-            		var carrelloButton = document.createElement('a');
-            		carrelloButton.href = 'ControlloProdotto?action=addToC&id='+ prodotto.id +'&quantity=1'; // Inserisci qui il link appropriato per il carrello
-            		carrelloButton.textContent = 'Carrello';
-            		carrelloButton.classList.add('carrello-button');
+            var prodotti = JSON.parse(xhr.responseText);
             
-            		details.appendChild(price);
-            		details.appendChild(carrelloButton);
-            		productcontainer.appendChild(img);
-            		productcontainer.appendChild(title);
-            		productcontainer.appendChild(details);
+            prodotti.forEach(function(prodotto) {
+                // Creazione del div principale della carta del prodotto
+                var productCard = document.createElement('div');
+                productCard.classList.add('product-card');
+                
+                var productContainer = document.createElement('div');
+                productContainer.classList.add('product-container');
+                productContainer.setAttribute('onclick', 'redirectToProduct(' + prodotto.id + ')');
+                
+                // Creazione del fronte della carta
+                var productFront = document.createElement('div');
+                productFront.classList.add('product-front');
+                
+                var img = document.createElement('img');
+                img.classList.add('product-image');
+                img.src = prodotto.immagine;
+                img.alt = prodotto.nome;
+                
+                var title = document.createElement('h3');
+                title.textContent = prodotto.nome;
+                title.classList.add('nome');
+                
+                var details = document.createElement('div');
+                details.classList.add('details');
+                
+                var price = document.createElement('p');
+                price.classList.add('price');
+                price.textContent = prodotto.prezzo.toFixed(2) + '€';
+                
+                details.appendChild(price);
+                
+                productFront.appendChild(img);
+                productFront.appendChild(title);
+                productFront.appendChild(details);
+                
+                // Creazione del retro della carta
+                var productBack = document.createElement('div');
+                productBack.classList.add('product-back');
+                
+                var productInfo = document.createElement('div');
+                productInfo.classList.add('product-info');
+                
+                // Funzione per creare paragrafi
+                var createParagraph = function(label, value) {
+                    var p = document.createElement('p');
+                    var b = document.createElement('b');
+                    b.textContent = label;
+                    p.appendChild(b);
+                    p.appendChild(document.createTextNode(' ' + value));
+                    return p;
+                };
 
-            // Aggiunta del prodotto creato al contenitore principale
-            		grid.appendChild(productcontainer);
-            		container.appendChild(grid);
-        			});
-                }
-            };
-            xhr.send();
+                productInfo.appendChild(createParagraph('Tipologia:', prodotto.tipologia));
+                productInfo.appendChild(createParagraph('Provenienza:', prodotto.provenienza));
+                productInfo.appendChild(createParagraph('Denominazione:', prodotto.denominazione));
+                productInfo.appendChild(createParagraph('Annata:', prodotto.annata));
+                productInfo.appendChild(createParagraph('Alcol:', prodotto.alcol + ' %'));
+                productInfo.appendChild(createParagraph('Formato:', prodotto.formato + ' cl'));
+                
+                var carrelloButton = document.createElement('a');
+                carrelloButton.href = 'ControlloProdotto?action=addToC&id=' + prodotto.id + '&quantity=1';
+                carrelloButton.textContent = 'Aggiungi al Carrello';
+                carrelloButton.classList.add('carrello-button');
+                
+                productBack.appendChild(productInfo);
+                productBack.appendChild(carrelloButton);
+                
+                productContainer.appendChild(productFront);
+                productContainer.appendChild(productBack);
+                productCard.appendChild(productContainer);
+                grid.appendChild(productCard);
+            });
+            
+            container.appendChild(grid);
+        }
+    };
+    xhr.send();
 }
-function ricerca(event){
-			event.preventDefault();
-            var xhr = new XMLHttpRequest();
-            var patternInput = document.getElementById("searchbar");
-            pattern = patternInput.value.trim();
-            if(pattern !== ""){
-	            xhr.open("GET", "catalogoAjax?pattern=" + pattern, true);
-	            xhr.onreadystatechange = function() {
-	                if (xhr.readyState === 4 && xhr.status === 200) {
-						var container = document.getElementById('container');
-			            container.innerHTML="";
-			            var grid = document.createElement('div');
-						grid.classList.add('grid-container');
-	                    var prodotti = JSON.parse(xhr.responseText);
-	                    prodotti.forEach(function(prodotto){
-						var productcontainer = document.createElement("div");
-						productcontainer.classList.add("product-container");
-						productcontainer.addEventListener('click',function() {
-					        redirectToProduct(prodotto.id);
-					    });
-						
-	            // Creazione del div principale del prodotto
-	
-	            // Creazione dell'elemento immagine
-	            		var img = document.createElement('img');
-	            		img.classList.add("product-image");
-	            		img.src = prodotto.immagine;
-	            		img.alt = prodotto.nome;
-	
-	            // Creazione del titolo (h3)
-	           			var title = document.createElement('h3');
-	            		title.textContent = prodotto.nome;
-	            		title.classList.add("nome");
-					
-						var details = document.createElement('div');
-						details.classList.add('details');	
-				
-	            // Creazione del paragrafo per il prezzo
-	            		var price = document.createElement('p');
-	            		price.classList.add("price");
-	            		price.textContent = prodotto.prezzo.toFixed(2) +"€ ";
-	            		details.appendChild(price);
-	
-	            // Creazione del link per il pulsante 'Carrello'
-	            		var carrelloButton = document.createElement('a');
-	            		carrelloButton.href = 'ControlloProdotto?action=addToC&id='+ prodotto.id +'&quantity=1'; // Inserisci qui il link appropriato per il carrello
-	            		carrelloButton.textContent = 'Carrello';
-	            		carrelloButton.classList.add('carrello-button');
-	            
-	            		details.appendChild(price);
-	            		details.appendChild(carrelloButton);
-	            		productcontainer.appendChild(img);
-	            		productcontainer.appendChild(title);
-	            		productcontainer.appendChild(details);
-	
-	            // Aggiunta del prodotto creato al contenitore principale
-	            		grid.appendChild(productcontainer);
-	            		container.appendChild(grid);
-	        			});
-	                }
-	            };
-	            xhr.send();
-	           }
+
+function ricerca(event) {
+    event.preventDefault();
+    var xhr = new XMLHttpRequest();
+    var patternInput = document.getElementById("searchbar");
+    var pattern = patternInput.value.trim();
+    
+    if (pattern !== "") {
+        xhr.open("GET", "catalogoAjax?pattern=" + pattern, true);
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState === 4 && xhr.status === 200) {
+                var container = document.getElementById('container');
+                container.innerHTML = ""; // Pulisce il contenuto esistente
+
+                var grid = document.createElement('div');
+                grid.classList.add('grid-container');
+
+                var prodotti = JSON.parse(xhr.responseText);
+                
+                prodotti.forEach(function(prodotto) {
+                    // Creazione del div principale del prodotto
+                    var productCard = document.createElement('div');
+                    productCard.classList.add('product-card');
+                    
+                    var productContainer = document.createElement('div');
+                    productContainer.classList.add('product-container');
+                    productContainer.setAttribute('onclick', 'redirectToProduct(' + prodotto.id + ')');
+
+                    // Creazione del fronte della carta
+                    var productFront = document.createElement('div');
+                    productFront.classList.add('product-front');
+                    
+                    var img = document.createElement('img');
+                    img.classList.add('product-image');
+                    img.src = prodotto.immagine;
+                    img.alt = prodotto.nome;
+                    
+                    var title = document.createElement('h3');
+                    title.textContent = prodotto.nome;
+                    title.classList.add('nome');
+                    
+                    var details = document.createElement('div');
+                    details.classList.add('details');
+                    
+                    var price = document.createElement('p');
+                    price.classList.add('price');
+                    price.textContent = prodotto.prezzo.toFixed(2) + '€';
+                    
+                    details.appendChild(price);
+                    
+                    productFront.appendChild(img);
+                    productFront.appendChild(title);
+                    productFront.appendChild(details);
+                    
+                    // Creazione del retro della carta
+                    var productBack = document.createElement('div');
+                    productBack.classList.add('product-back');
+                    
+                    var productInfo = document.createElement('div');
+                    productInfo.classList.add('product-info');
+                    
+                    var tipologia = document.createElement('p');
+                    tipologia.innerHTML = `<b>Tipologia: </b>${prodotto.tipologia}`;
+                    
+                    var provenienza = document.createElement('p');
+                    provenienza.innerHTML = `<b>Provenienza: </b>${prodotto.provenienza}`;
+                    
+                    var denominazione = document.createElement('p');
+                    denominazione.innerHTML = `<b>Denominazione: </b>${prodotto.denominazione}`;
+                    
+                    var annata = document.createElement('p');
+                    annata.innerHTML = `<b>Annata: </b>${prodotto.annata}`;
+                    
+                    var alcol = document.createElement('p');
+                    alcol.innerHTML = `<b>Alcol: </b>${prodotto.alcol} %`;
+                    
+                    var formato = document.createElement('p');
+                    formato.innerHTML = `<b>Formato: </b>${prodotto.formato} cl`;
+
+                    productInfo.appendChild(tipologia);
+                    productInfo.appendChild(provenienza);
+                    productInfo.appendChild(denominazione);
+                    productInfo.appendChild(annata);
+                    productInfo.appendChild(alcol);
+                    productInfo.appendChild(formato);
+
+                    var carrelloButton = document.createElement('a');
+                    carrelloButton.href = 'ControlloProdotto?action=addToC&id=' + prodotto.id + '&quantity=1';
+                    carrelloButton.textContent = 'Aggiungi al Carrello';
+                    carrelloButton.classList.add('carrello-button');
+                    
+                    productBack.appendChild(productInfo);
+                    productBack.appendChild(carrelloButton);
+                    
+                    productContainer.appendChild(productFront);
+                    productContainer.appendChild(productBack);
+                    productCard.appendChild(productContainer);
+                    grid.appendChild(productCard);
+                });
+                
+                container.appendChild(grid);
+            }
+        };
+        xhr.send();
+    }
 }
 
 
